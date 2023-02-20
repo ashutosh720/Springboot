220201f7420f652ab64217447fb98777957595d7
ashutosh720 <112875409+ashutosh720@users.noreply.github.com>
2023-02-20 17:36:08 +0530

    Delete ECommerce directory

--- ECommerce/src/main/java/com/example/ECommerce/Service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ECommerce.Service;
-
-import com.example.ECommerce.Model.Order;
-
-public interface OrderService {
-
-    Order getOrderById(Integer orderId);
-    Order createOrder(Order order);
-}
