89b46aa03682f40961dfc0934bf06ea029252df7
ashutosh720 <112875409+ashutosh720@users.noreply.github.com>
2023-04-23 23:04:28 +0530

    Update RestaurantController.java

--- restaurant_model/src/main/java/com/example/restaurant_model/Controller/RestaurantController.java
+++ restaurant_model/src/main/java/com/example/restaurant_model/Controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/restaurant-model")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
